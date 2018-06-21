3718de265b1f843aed38ceb2addcb7b8b98592a2
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-21 08:00:03 +0200

    Move test to component folder

--- src/components/ToDoApp/ToDoApp.test.js
+++ src/components/ToDoApp/ToDoApp.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ToDoApp from './index';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<ToDoApp />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
