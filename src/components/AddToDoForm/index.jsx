86ad1ab3c3eb4b2ee139aadcc66f0738324525df
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-21 08:01:11 +0200

    Change propTypes

--- src/components/AddToDoForm/index.jsx
+++ src/components/AddToDoForm/index.jsx
@@ -6,7 +6,7 @@ import Button from '../Button';
 const propTypes = {
   addToDo: PropTypes.func.isRequired,
   handleChange: PropTypes.func.isRequired,
-  toDoValue: PropTypes.func.isRequired
+  toDoValue: PropTypes.string.isRequired
 };
 
 const AddToDoForm = ({ addToDo, handleChange, toDoValue }) => (
