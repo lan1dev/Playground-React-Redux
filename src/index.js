b324bf824e6c253bea116584a35852d7b043ae08
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2018-06-21 04:22:58 +0200

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ToDoApp from './components/ToDoApp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<ToDoApp />, document.getElementById('root'));
